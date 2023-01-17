c731d25541d308756499f1bc170e3cf6546b2179
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2023-01-18 08:34:50 +0900

    #1 submit failed
    study and implementation required

--- BOJ/1260.py
+++ BOJ/1260.py
@@ -2,46 +2,47 @@ import sys
 
 input = sys.stdin.readline
 
-dfs_str = ''
-bfs_str = ''
+dfs_values = []
+bfs_values = []
 
 class Node:
-  def __init__(self, v):
-    self.v = v
-    self.visited = False
-    self.adjacent = []
+    def __init__(self, v):
+        self.v = v
+        self.visited = False
+        self.adjacent = []
 
-  def visit(self):
-    self.visited = True
+    def visit(self):
+        self.visited = True
 
 
+# different result compared with example
 def dfs(root):
-  if root is None:
-    return
-  root.visit()
-  dfs_str += root.v + ' '
+    if root is None:
+        return
+    root.visit()
+    dfs_values.append(root.v)
 
-  for node in root.adjacent:
-    if not node.visited:
-      dfs(node)
+    for node in root.adjacent:
+        if not node.visited:
+            dfs(node)
 
-
+# not even traverse all adjacent
 def bfs(root):
-  q = []
-  root.visit()
-  bfs_str += root.v + ' '
-  for i in root.adjacent:
-    q.append(i) 
-  idx = 0
+    q = []
+    root.visit()
+    bfs_values.append(root.v)
+    for i in root.adjacent:
+        q.append(i) 
+    idx = 0
   
-  while len(q) > idx:
-    bfs_str += q[idx].v + ' '
-    for node in q[idx].adjacent:
-      if not node.visited:
-        node.visited = True
-        q.append(node)
+    while len(q) > idx:
+        bfs_values.append(q[idx].v)
+        for node in q[idx].adjacent:
+            if not node.visited:
+                node.visited = True
+                q.append(node)
     
-    idx += 1
+        idx += 1
 
 
 n, m, v = list(map(int, input().split(' ')))
@@ -50,11 +51,19 @@ nodes = [Node(i) for i in range(n + 1)]
 
 
 for i in range(m):
-  i, j = list(map(int, input().split(' ')))
-  nodes[i].adjacent.append(nodes[j])
+    i, j = list(map(int, input().split(' ')))
+    nodes[i].adjacent.append(nodes[j])
+    # one way adjacent
 
 dfs(nodes[v])
 bfs(nodes[v])
 
-print(dfs_str)
-print(bfs_str)
+result = str(dfs_values[0])
+for i in dfs_values[1:]:
+    result += ' ' + str(i)
+print(result)
+
+result = str(bfs_values[0])
+for i in bfs_values[1:]:
+    result += ' ' + str(i)
+print(result)
