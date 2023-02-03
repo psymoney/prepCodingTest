4835462382e47b52965025ec5f843c3370c9a513
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2023-02-03 12:23:11 +0900

    solve 1890

--- BOJ/1890.py
+++ BOJ/1890.py
@@ -20,3 +20,27 @@ def retrieve(i, j):
     return routes
 
 print(retrieve(0, 0))
+
+# SOLVE
+# DP approach
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * N for _ in range(N)]
+
+dp[0][0] = 1
+
+for i in range(N):
+    for j in range(N):
+        num = arr[i][j]
+        if i == N - 1 and j == N - 1:
+            print(dp[-1][-1])
+            break
+        if dp[i][j] == 0:
+            continue
+        if j + num < N:
+            dp[i][j + num] += dp[i][j]
+        if i + num < N:
+            dp[i + num][j] += dp[i][j]
