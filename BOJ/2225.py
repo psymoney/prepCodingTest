39c401982658eceb3535aae7f1669f86f3ddbc3d
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2023-02-17 13:39:57 +0900

    solve 2225

--- BOJ/2225.py
+++ BOJ/2225.py
@@ -5,6 +5,6 @@ dp = [[0] * (N+1)] + [[1] + [0] * N for _ in range(K)]
 
 for i in range(1, K + 1):
     for j in range(1, N + 1):
-        dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000
 
 print(dp[-1][-1])
