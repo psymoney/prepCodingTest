d60820843f989b2915aef6bfefd72f605582ab1e
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2023-02-20 07:32:01 +0900

    second fail: out of memory

--- BOJ/5582.py
+++ BOJ/5582.py
@@ -1,36 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-def fill_part_strs(str, part_strs, max_com_len):
-    for i in range(len(str)):
-        for (l, j) in enumerate(range(i+1, max_com_len)):
-            # print(f'i = {i} / j = {j} / str_1[i:j] = {str_1[i:j]} / l = {l}')
-            try:
-                part_strs[l+1].index(str[i:j])
-                continue
-            except ValueError:
-                part_strs[l+1].append(str[i:j])
+str_1 = input().replace("\n", "")
+str_2 = input().replace("\n", "")
 
-def solution(part_strs_1, part_strs_2, max_com_len):
-    for i in range(max_com_len - 1, -1, -1):
-        for j in range(len(part_strs_1[i])):
-            try:
-                part_strs_2[i].index(part_strs_1[i][j])
-                return i
+dp = [[0] * (len(str_2) + 1) for _ in range(len(str_1) + 1)]
+max = 0
+for i in range(len(str_1)):
+    for j in range(len(str_2)):
+        if str_1[i] == str_2[j]:
+            v = dp[i][j] + 1
+            max = v if v > max else max
+            dp[i+1][j+1] = v
 
-            except ValueError:
-                continue
-
-
-str_1 = input()
-str_2 = input()
-
-max_com_len = len(str_1) if len(str_1) < len(str_2) else len(str_2)
-part_strs_1 = [[' ']] + [[] for _ in range(max_com_len - 1)]
-part_strs_2 = [[' ']] + [[] for _ in range(max_com_len - 1)]
-
-fill_part_strs(str_1, part_strs_1, max_com_len)
-fill_part_strs(str_2, part_strs_2, max_com_len)
-
-print(solution(part_strs_1, part_strs_2, max_com_len))
-
+print(max)
