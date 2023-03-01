18bcfdaeab101064e7889de517e693788b842973
Sangyeop Kim <sykim@amcryptoteam.com>
2023-03-01 17:58:05 +0900

    solve 9205

--- BOJ/9205.py
+++ BOJ/9205.py
@@ -1,46 +1,35 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-def initiate_nodes_and_adjacent(nodes: list, adj: list, n: int) -> None:
-    for i in range(n):
-        nodes[i] = list(map(int, input().split()))
-        nodes[i].append(0)
 
-    for i in range(n - 1):
-        for j in range(i + 1, n):
-            x1, y1, v = nodes[i]
-            x2, y2, v = nodes[j]
-            dist = abs(x2 - x1) + (y2 - y1)
-            if dist <= 1000:
-                adj[i].append(j)
-                adj[j].append(i)
-
-
-def bfs(nodes: list, adj: list, n: int) -> None:
-    Q = adj[0]
-    nodes[0][2] = 1
+def bfs(home: list, stores: list, festival: list) -> str:
+    visited = [0] * len(stores)
+    Q = deque()
+    Q.append(home)
+    result = 'sad'
     while Q:
-        i = Q.pop()
-        if i == n - 1:
-            return
-        for a in adj[i]:
-            if nodes[a][2] == 0:
-                nodes[a][2] = 1
-                Q.append(a)
+        x1, y1 = Q.popleft()
+        if abs(x1 - festival[0]) + abs(y1 - festival[1]) <= 1000:
+            return 'happy'
+        for i in range(len(stores)):
+            x2, y2 = stores[i]
+            dist = abs(x2 - x1) + abs(y2 - y1)
+            if dist <= 1000 and visited[i] == 0:
+                Q.append(stores[i])
+                visited[i] = 1
+    return result
 
 
 def test() -> str:
-    n = int(input()) + 2
-    nodes = [[] for _ in range(n)]
-    adj = [[] for _ in range(n)]
-
-    initiate_nodes_and_adjacent(nodes, adj, n)
-    bfs(nodes, adj, n)
-
-    return 'happy' if nodes[-1][-1] == 1 else 'sad'
+    n = int(input())
+    home = list(map(int, input().split()))
+    stores = [list(map(int, input().split())) for _ in range(n)]
+    festival = list(map(int, input().split()))
+    print(bfs(home, stores, festival))
 
 
 if __name__ == '__main__':
     t = int(input())
     for _ in range(t):
-        print(test())
+        test()
