a626761d52540f93eed560cc9dfb13c22da3dd9f
Sangyeop Kim <sykim@amcryptoteam.com>
2023-03-03 13:35:54 +0900

    fisrt fail

--- BOJ/2573.py
+++ BOJ/2573.py
@@ -3,57 +3,51 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 G = [list(map(int, input().split())) for _ in range(N)]
-V = [[True] * M] + [([True] + [0] * (M - 2) + [True]) for _ in range(N)] + [[True] * M]
 D = [(0, -1), (-1, 0), (0, 1), (1, 0)]
 Q = []
-print(V)
-
-# first approach: check everything while traverse
-year = 0
-while True:
-    continents = 0
-    for y in range(1, N - 1):
-        for x in range(1, M - 1):
-            # need to check the case all 0
-            if V[y][x]:
-                continue
-            V[y][x] = True
-            if G[y][x] != 0:
-                continents += 1
-                if continents >= 2:
-                    print(year)
-                    quit()
-                # count adjacent
-                Q = []
-                adj = 0
-                for dx, dy in D:
-                    nx, ny = x + dx, y + dy
-                    if G[ny][nx] == 0:
-                        adj += 1
-                        continue
-                    if G[ny][nx] != 0 and not V[ny][nx]:
-                        V[ny][nx] = True
-                        Q.append((nx, ny))
-                G[y][x] -= adj if adj <= G[y][x] else 0
-                while Q:
-
-                pass
-        pass
 
 # second approach: decrease glacial while traversing graph, and then check the glacial is separated
 year = 0
 while True:
+    # check the glacial is scattered
+    V = [[True] * M] + [([True] + [0] * (M - 2) + [True]) for _ in range(N - 2)] + [[True] * M]
+    bunches = 0
+    for y in range(1, N - 1):
+        for x in range(1, M - 1):
+            if G[y][x] == 0:
+                V[y][x] = True
+            if G[y][x] != 0 and not V[y][x]:
+                V[y][x] = True
+                Q = [(x, y)]
+                bunches += 1
+                if bunches >= 2:
+                    print(year)
+                    quit()
+                while Q:
+                    x2, y2 = Q.pop()
+                    for dx, dy in D:
+                        nx, ny = x2 + dx, y2 + dy
+                        if 0 <= nx < M and 0 <= ny < N and not V[ny][nx] and G[ny][nx] > 0:
+                            Q.append((nx, ny))
+                            V[ny][nx] = True
+    if bunches == 0:
+        print(0)
+        quit()
+
     # traverse graph and melt the glacial
-    # time pass by 1 year
-    while Q:
-        x, y = Q.pop()
-        if G[y][x] != 0:
-            # count adjacent 0
-            pass
+    for y in range(1, N - 1):
+        for x in range(1, M - 1):
+            if G[y][x] == 0:
+                continue
+            num_seas = 0
+            for dx, dy in D:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < M and 0 <= ny < N and G[ny][nx] == 0:
+                    num_seas += 1
+            G[y][x] = G[y][x] - num_seas if num_seas <= G[y][x] else 0
     year += 1
 
-    # check the glacial is scattered
-    # if yes then print -> year then finish the process
+
 
 
 
