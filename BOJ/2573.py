1ed5fc6abbd2b2e2dc7ed06033dab35e92c10f92
Sangyeop Kim <sykim@amcryptoteam.com>
2023-03-05 09:11:36 +0900

    solve 2573

--- BOJ/2573.py
+++ BOJ/2573.py
@@ -5,80 +5,44 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 G = [list(map(int, input().split())) for _ in range(N)]
 D = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-glacial = deque
 
-for y in range(1, N - 1):
-    for x in range(1, M - 1):
-        if G[y][x] > 0:
-            glacial.append((x, y))
-
-year = 1
-bunches = 0
-V = [[0] * M for _ in range(N)]
-while glacial:
-    x, y = glacial.popleft()
-    if V[y][x] != 0 or G[y][x] == 0:
-        continue
-
-    seas = 0
+def dfs(m, n, V, G, NG):
     Q = deque()
-    for dx, dy in D:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < M and 0 <= ny < N:
-            if G[ny][nx] == 0:
-                seas += 1
-            else:
-                Q.append((nx, ny))
-    V[y][x] = seas
+    Q.append((m, n))
 
+    while Q:
+        x, y = Q.popleft()
+        shores = 0
+        for dx, dy in D:
+            nx, ny = x + dx, y + dy
 
+            if 0 <= nx < M and 0 <= ny < N:
+                if G[ny][nx] == 0:
+                    shores += 1
+                elif G[ny][nx] > 0 and not V[ny][nx]:
+                    Q.append((nx, ny))
+                    V[ny][nx] = True
+        NG[y][x] = max(0, G[y][x] - shores)
+    return 1
 
-
-
-
+year = 0
+while True:
+    V = [[False for _ in range(M)] for _ in range(N)]
+    NG = [[0] * M for _ in range(N)]
+    num_glacial = 0
     for y in range(1, N - 1):
         for x in range(1, M - 1):
             if G[y][x] == 0:
                 continue
-            if V[y][x] != 0:
+            if V[y][x]:
                 continue
-
-            num_seas = 0
-            for dx, dy in D:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < M and 0 <= ny < N and G[ny][nx] == 0:
-                    num_seas += 1
-            V[y][x] = G[y][x] - num_seas if num_seas <= G[y][x] else 0
-
-            Q = deque()
-            Q.append((x, y))
-            bunches += 1
-            if bunches >= 2:
-                print(year)
-                quit()
-            while Q:
-                x2, y2 = Q.popleft()
-                for dx, dy in D:
-                    nx, ny = x2 + dx, y2 + dy
-                    if 0 <= nx < M and 0 <= ny < N and V[ny][nx] == 0 and G[ny][nx] > 0:
-                        Q.append((nx, ny))
-
-                        num_seas = 0
-                        for dx2, dy2 in D:
-                            nx2, ny2 = x2 + dx2, y2 + dy2
-                            if 0 <= nx2 < M and 0 <= ny2 < N and G[ny2][nx2] == 0:
-                                num_seas += 1
-
-                        V[ny][nx] = G[ny][nx] - num_seas if num_seas <= G[ny][nx] else 0
-    if bunches == 0:
-        print(0)
-        quit()
-    G = V
+            num_glacial += dfs(x, y, V, G, NG)
+    if num_glacial > 1:
+        break
+    if num_glacial == 0:
+        year = 0
+        break
+    G = NG
     year += 1
 
-
-
-
-
-
-
+print(year)
