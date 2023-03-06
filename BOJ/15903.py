5fe6ad92c3c4905da4ef4fbd97d73089bee90f20
Sangyeop Kim <sykim@amcryptoteam.com>
2023-03-07 07:34:38 +0900

    solve with heapq: faster

--- BOJ/15903.py
+++ BOJ/15903.py
@@ -1,12 +1,15 @@
 import sys
+import heapq
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 cards = list(map(int, input().split()))
+heapq.heapify(cards)
 
 for _ in range(m):
-    cards.sort()
-    union = cards[0] + cards[1]
-    cards[0], cards[1] = union, union
+    a = heapq.heappop(cards)
+    b = heapq.heappop(cards)
+    heapq.heappush(cards, a + b)
+    heapq.heappush(cards, a + b)
 
 print(sum(cards))
