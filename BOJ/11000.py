42dbda2190d77ec46819f4de80e8fdb4ef64d06d
Sangyeop Kim <sykim@amcryptoteam.com>
2023-03-08 12:28:09 +0900

    solve 11000

--- BOJ/11000.py
+++ BOJ/11000.py
@@ -1,16 +1,22 @@
 import sys
+from heapq import *
 input = sys.stdin.readline
 
 N = int(input())
 classes = sorted([tuple(map(int, input().split())) for _ in range(N)], key=lambda x: x[0])
-classrooms = [classes[0][1]]
-earliest = classrooms[0]
+classrooms = []
+heapify(classrooms)
+earliest = classes[0][1]
 
-for i in range(1, N):
-    if classes[i][0] < earliest:
-        classrooms.append(classes[i][1])
+for s, t in classes[1:]:
+    if s < earliest:
+        if t <= earliest:
+            heappush(classrooms, earliest)
+            earliest = t
+        else:
+            heappush(classrooms, t)
     else:
-        classrooms[classrooms.index(earliest)] = classes[i][1]
-    earliest = min(classrooms)
+        heappush(classrooms, t)
+        earliest = heappop(classrooms)
 
-print(len(classrooms))
+print(len(classrooms) + 1)
