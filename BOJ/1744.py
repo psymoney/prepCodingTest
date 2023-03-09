1567ebf8f57ed75caf52a67344a5ad1508d32a3d
Sangyeop Kim <sykim@amcryptoteam.com>
2023-03-09 17:22:14 +0900

    improve logic

--- BOJ/1744.py
+++ BOJ/1744.py
@@ -5,17 +5,15 @@ input = sys.stdin.readline
 N = int(input())
 PQ = []
 MQ = []
-zeros = []
+max_sum = 0
 for _ in range(N):
     n = int(input())
     if n > 0:
         heappush(PQ, -n)
-    elif n == 0:
-        zeros.append(0)
+    elif n == 1:
+        max_sum += 1
     else:
         heappush(MQ, n)
-
-max_sum = 0
 
 while len(PQ) > 1:
     a, b = heappop(PQ) * (-1), heappop(PQ) * (-1)
@@ -24,9 +22,6 @@ while len(PQ) > 1:
 while len(MQ) > 1:
     a, b = heappop(MQ), heappop(MQ)
     max_sum += max(a * b, a + b)
-
-if len(MQ) > 0 and len(zeros) > 0:
-    heappop(MQ)
 
 if len(PQ) == 1:
     pq = heappop(PQ) * (-1)
