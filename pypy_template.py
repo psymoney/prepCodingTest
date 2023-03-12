f20649fecf3cb8ef5c1856837fe0098f0da2b8fa
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2023-03-13 08:19:01 +0900

    Create pypy_template.py
    reference: https://www.acmicpc.net/source/46822818
    credit to tk4003950

---
+++ pypy_template.py
@@ -0,0 +1,94 @@
+# CP template Version 1.006
+import io
+import os
+import sys
+#import string
+#from functools import cmp_to_key, reduce, partial
+#import itertools
+#from itertools import product
+#import collections
+#from collections import deque
+#from collections import Counter, defaultdict as dd
+#import math
+#from math import log, log2, ceil, floor, gcd, sqrt
+#from heapq import heappush, heappop
+#import bisect
+#from bisect import bisect_left as bl
+DEBUG = False
+
+def main(f=None):
+    init(f)
+    # sys.setrecursionlimit(10**9)
+    # ######## INPUT AREA BEGIN ##########
+    
+    count = [0] * (int(input().strip()) + 1)
+    for el in map(int, input().split()):
+        count[el] = count[el-1] + 1
+    return(len(count) - max(count) - 1)
+
+    # ######## INPUT AREA END ############
+
+
+# TEMPLATE ###############################
+
+
+enu = enumerate
+
+
+def For(*args):
+    return itertools.product(*map(range, args))
+
+
+def Mat(h, w, default=None):
+    return [[default for _ in range(w)] for _ in range(h)]
+
+
+def nDim(*args, default=None):
+    if len(args) == 1:
+        return [default for _ in range(args[0])]
+    else:
+        return [nDim(*args[1:], default=default) for _ in range(args[0])]
+
+
+def setStdin(f):
+    global DEBUG, input
+    DEBUG = True
+    sys.stdin = open(f)
+    input = sys.stdin.readline
+
+
+def init(f=None):
+    global input
+    input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
+    if os.path.exists("o"):
+        sys.stdout = open("o", "w")
+    if f is not None:
+        setStdin(f)
+    else:
+        if len(sys.argv) == 1:
+            if os.path.isfile("in/i"):
+                setStdin("in/i")
+            elif os.path.isfile("i"):
+                setStdin("i")
+        elif len(sys.argv) == 2:
+            setStdin(sys.argv[1])
+        else:
+            assert False, "Too many sys.argv: %d" % len(sys.argv)
+
+
+def pr(*args):
+    if DEBUG:
+        print(*args)
+
+
+def pfast(*args, end="\n", sep=' '):
+    sys.stdout.write(sep.join(map(str, args)) + end)
+
+
+def parr(arr):
+    for i in arr:
+        print(i)
+
+
+if __name__ == "__main__":
+    print(main())
