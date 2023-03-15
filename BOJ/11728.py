97cb963c1a0c14de72f843b80f8492110ca684e4
Sangyeop Kim <sykim@amcryptoteam.com>
2023-03-15 12:33:45 +0900

    solve 11728

--- BOJ/11728.py
+++ BOJ/11728.py
@@ -5,10 +5,4 @@ N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 L = sorted(A + B)
-result = ''
-for i in range(N + M):
-    if i == N + M - 1:
-        result += str(L[i])
-    else:
-        result += str(L[i]) + ' '
-print(result)
+print(' '.join(str(x) for x in L))
