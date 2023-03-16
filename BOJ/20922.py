2053b4a1b66139a7aaa6afb09926c8a7b96ca2b3
Sangyeop Kim <sykim@amcryptoteam.com>
2023-03-16 15:20:51 +0900

    solve 20922

--- BOJ/20922.py
+++ BOJ/20922.py
@@ -1,16 +1,23 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 N, K = map(int, input().split())
-records = [0] * (100001)
+records = [deque([]) for _ in range(100001)]
 numbers = list(map(int, input().split()))
 max_partial_length = 0
 cur_partial_length = 0
-for number in numbers:
-    if records[number] == K:
-        max_partial_length = max(max_partial_length, cur_partial_length)
-        cur_partial_length = 0
-        records = [0] * (100001)
-    records[number] += 1
+cur_char_index = 0
+for idx in range(N):
+    num = numbers[idx]
+    if len(records[num]) == K:
+        last_idx = records[num].popleft()
+        if last_idx < cur_char_index:
+            pass
+        else:
+            max_partial_length = max(max_partial_length, cur_partial_length)
+            cur_partial_length = idx - last_idx - 1
+            cur_char_index = last_idx + 1
+    records[num].append(idx)
     cur_partial_length += 1
 max_partial_length = max(max_partial_length, cur_partial_length)
 print(max_partial_length)
