2be4975d64722b8087e3afa0fbdb7b12056aaf56
Sangyeop Kim <sykim@amcryptoteam.com>
2023-03-17 08:06:43 +0900

    improve 20922
    1. add self-assertion with test cases
    2. improve get_answer logic: time-wise from 428ms to 124ms, memory-wise from 131,700 KB(~130 MB) to 53,380 KB(~52 MB)

--- BOJ/20922.py
+++ BOJ/20922.py
@@ -1,23 +1,54 @@
 import sys
-from collections import deque
-input = sys.stdin.readline
-N, K = map(int, input().split())
-records = [deque([]) for _ in range(100001)]
-numbers = list(map(int, input().split()))
-max_partial_length = 0
-cur_partial_length = 0
-cur_char_index = 0
-for idx in range(N):
-    num = numbers[idx]
-    if len(records[num]) == K:
-        last_idx = records[num].popleft()
-        if last_idx < cur_char_index:
-            pass
-        else:
-            max_partial_length = max(max_partial_length, cur_partial_length)
-            cur_partial_length = idx - last_idx - 1
-            cur_char_index = last_idx + 1
-    records[num].append(idx)
-    cur_partial_length += 1
-max_partial_length = max(max_partial_length, cur_partial_length)
-print(max_partial_length)
+
+def get_answer(N, K, numbers):
+    records = [0] * 100001
+    max_partial_length = 0
+    left_idx = 0
+
+    for right_idx in range(N):
+        num = numbers[right_idx]
+        if records[num] == K:
+            max_partial_length = max(max_partial_length, right_idx - left_idx)
+            while numbers[left_idx] != numbers[right_idx]:
+                records[numbers[left_idx]] -= 1
+                left_idx += 1
+            left_idx += 1
+            records[num] -= 1
+        records[num] += 1
+
+    max_partial_length = max(max_partial_length, N - left_idx)
+    return max_partial_length
+
+def sol():
+    input = sys.stdin.readline
+    N, K = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    answer = get_answer(N, K, numbers)
+    print(answer)
+
+def test():
+    TEST_CASES = [
+        ["9 2", "3 2 5 5 6 4 4 5 7", 7],
+        ["10 1", "1 2 3 4 5 6 6 7 8 9", 6],
+        ["29 3", "1 2 3 4 5 6 7 8 9 1 1 11 1 14 15 16 23 43 24 53 24 25 99 29 36 45 64 69 45", 28],
+        ["16 3", "1 2 3 4 5 6 7 8 9 1 1 11 1 2 2 2", 14],
+        ["10 3", "1 2 3 4 5 6 7 1 1 1", 9],
+        ["10 3", "1 1 1 1 1 1 1 1 1 1", 3],
+        ["10 3", "1 1 1 2 2 1 1 1 1 1", 5],
+        ["21 1", "1 2 3 4 5 6 6 7 8 9 10 11 13 15 63 34 34 33 33 22 1", 10],
+        ["21 2", "1 2 3 2 3 1 4 4 4 5 6 7 8 9 9 8 7 6 5 2 1", 14]
+    ]
+    failed_case = 0
+    for (i, case) in enumerate(TEST_CASES):
+        N, K = map(int, case[0].split())
+        numbers = list(map(int, case[1].split()))
+        expected_answer = int(case[2])
+        answer = get_answer(N, K, numbers)
+        if answer != expected_answer:
+            print(f'#{i+1} test case has been failed: the expected answer is {expected_answer} but {answer} is given')
+            failed_case += 1
+    if failed_case > 0:
+        exit(0)
+
+test()
+sol()
