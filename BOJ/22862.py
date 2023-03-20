441f513622ae13dc41c41827d0c2452dd043426e
Sangyeop Kim <sykim@amcryptoteam.com>
2023-03-20 13:24:45 +0900

    add test cases

--- BOJ/22862.py
+++ BOJ/22862.py
@@ -1,7 +1,7 @@
 import sys
 
 def solution(N, K, series):
-    L, R = 0, 0
+    L = 0
     length = 0
     max_length = 0
     K_cnt = 0
@@ -14,7 +14,7 @@ def solution(N, K, series):
                     K_cnt -= 1
                 else:
                     length -= 1
-            L += 1
+                L += 1
         else:
             length += 1
             max_length = max(length, max_length)
@@ -34,12 +34,15 @@ def test():
         [5, 1, list(map(int, "2 1 2 1 1".split())), 2],
         [1, 1, [2], 1],
         [10, 1, list(map(int, "1 1 1 1 1 1 1 1 1 1".split())), 0],
-        [10, 5, list(map(int, "1 1 1 1 1 1 1 1 1 1".split())), 0]
+        [10, 5, list(map(int, "1 1 1 1 1 1 1 1 1 1".split())), 0],
+        [7, 3, list(map(int, "1 2 3 3 3 4 5".split())), 2],
+        [12, 3, list(map(int, "1 2 3 3 3 4 5 6 7 8 9 10".split())), 4],
+        [13, 3, list(map(int, "3 3 3 3 3 3 3 4 5 6 7 8 9 10".split())), 4]
     ]
 
     for idx, (N, K, series, answer) in enumerate(test_cases):
         result = solution(N, K, series)
-        print(f'N = {N}, K = {K}, series = {series}, answer = {answer}, result = {result}')
+        # print(f'N = {N}, K = {K}, series = {series}, answer = {answer}, result = {result}')
         if result != answer:
             print(f'case #{idx + 1} is wrong!\nanswer is {answer}, but {result} is given.')
         else:
