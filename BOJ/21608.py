264c5ad73f50e32c7acb59c17595c92355440b10
Sangyeop Kim <sangykimiv@gmail.com>
2023-08-06 22:27:27 +0900

    Refact: wrap by class

--- BOJ/21608.py
+++ BOJ/21608.py
@@ -1,112 +1,90 @@
 import sys
 
 
-input = sys.stdin.readline
+class SharkSchool:
+    N: int = None
+    students: list = None
+    table: list = None
+    satisfactions: list = None
 
-N = int(input())
-students = []
+    dr: list = [-1, 1, 0, 0]
+    dc: list = [0, 0, -1, 1]
 
-for i in range(N * N):
-    students.append(list(map(int, input().split())))
+    def __init__(self):
+        pass
 
-table = [[0] * N for _ in range(N)]
-satisfactions = [[0] * N for _ in range(N)]
+    def initiate_properties(self) -> None:
+        input = sys.stdin.readline
+        self.N = int(input())
+        self.students = [list(map(int, input().split())) for _ in range(self.N ** 2)]
+        self.table = [[0] * self.N for _ in range(self.N)]
+        self.satisfactions = [[0] * self.N for _ in range(self.N)]
 
+    def solve(self):
+        self.initiate_properties()
+        self.allocates()
+        print(self.total_satisfaction)
 
-def get_empty_adjacent_tables(row: int, col: int, t: list, n: int) -> int:
-    empty_adjacent_tables = 0
+    # O(N^4)
+    def allocates(self) -> None:
+        for std in self.students:                  # O(N^2)
+            self.allocate_student_at_table(std)    # O(N^2)
 
-    if row > 0 and t[row - 1][col] == 0:
-        empty_adjacent_tables += 1
-    if row < n - 1 and t[row + 1][col] == 0:
-        empty_adjacent_tables += 1
-    if col > 0 and t[row][col - 1] == 0:
-        empty_adjacent_tables += 1
-    if col < n - 1 and t[row][col + 1] == 0:
-        empty_adjacent_tables += 1
+    # O(N^4)
+    @property
+    def total_satisfaction(self) -> int:
+        total_satisfaction: int = 0
+        students: list = sorted(self.students)
 
-    return empty_adjacent_tables
+        for row in range(self.N):
+            for col in range(self.N):
+                idx = self.table[row][col] - 1
 
+                empty, interested = self.get_empty_and_interested_adjacent_tables(row, col, students[idx])
+                satisfaction = 10 ** (interested - 1) if interested != 0 else 0
 
-def get_interested_adjacent_tables(row: int, col: int, t: list, n: int, stu) -> int:
-    interested_adjacent_tables = 0
+                total_satisfaction += satisfaction
 
-    if row > 0 and t[row - 1][col] in stu[1:]:
-        interested_adjacent_tables += 1
-    if row < n - 1 and t[row + 1][col] in stu[1:]:
-        interested_adjacent_tables += 1
-    if col > 0 and t[row][col - 1] in stu[1:]:
-        interested_adjacent_tables += 1
-    if col < n - 1 and t[row][col + 1] in stu[1:]:
-        interested_adjacent_tables += 1
+        return total_satisfaction
 
-    return interested_adjacent_tables
+    # O(N^2)
+    def allocate_student_at_table(self, stu: list) -> None:
+        temp = (0, 0, 0, 0)  # respectively interested seats, empty seats, row, and column
 
+        for row in range(self.N):
+            for col in range(self.N):
+                if self.table[row][col] != 0:
+                    continue
 
-# O(N^2)
-def allocate_student_at_table(student_data: list, t: list, n) -> None:
-    temp = (0, 0, 0, 0)
+                empty_tables, interested_tables = self.get_empty_and_interested_adjacent_tables(row, col, stu)
 
-    for row in range(n):
-        for col in range(n):
-            if t[row][col] != 0:
-                continue
+                if interested_tables > temp[0]:
+                    temp = (interested_tables, empty_tables, row, col)
+                elif interested_tables == temp[0]:
+                    if empty_tables > temp[1]:
+                        temp = (interested_tables, empty_tables, row, col)
+                    elif empty_tables == temp[1]:
+                        if row < temp[2]:
+                            temp = (interested_tables, empty_tables, row, col)
+                        elif row == temp[2] and col < temp[3]:
+                            temp = (interested_tables, empty_tables, row, col)
 
-            empty_adjacent_tables = get_empty_adjacent_tables(row, col, t, n)
-            interested_adjacent_tables = get_interested_adjacent_tables(row, col, t, n, student_data)
+        self.table[temp[2]][temp[3]] = stu[0]
 
-            if interested_adjacent_tables > temp[0]:
-                temp = (interested_adjacent_tables, empty_adjacent_tables, row, col)
-            elif interested_adjacent_tables == temp[0]:
-                if empty_adjacent_tables > temp[1]:
-                    temp = (interested_adjacent_tables, empty_adjacent_tables, row, col)
-                elif empty_adjacent_tables == temp[1]:
-                    if row < temp[2]:
-                        temp = (interested_adjacent_tables, empty_adjacent_tables, row, col)
-                    elif row == temp[2] and col < temp[3]:
-                        temp = (interested_adjacent_tables, empty_adjacent_tables, row, col)
+    def get_empty_and_interested_adjacent_tables(self, row: int, col: int, std: list) -> tuple:
+        empty_tables, interested_tables = 0, 0
 
-    t[temp[2]][temp[3]] = student_data[0]
+        for i in range(4):
+            nr, nc = row + self.dr[i], col + self.dc[i]
 
+            if 0 <= nr < self.N and 0 <= nc < self.N:
+                if self.table[nr][nc] == 0:
+                    empty_tables += 1
+                elif self.table[nr][nc] in std[1:]:
+                    interested_tables += 1
 
-# O(N^4)
-def allocates(std, t, n) -> None:
-    for student in std:                             # O(N^2)
-        allocate_student_at_table(student, t, n)    # O(N^2)
-
-
-#O(N^4)
-def calculate_satisfaction_table(std, t, n, s) -> None:
-    total = 0
-    for row in range(n):
-        for col in range(n):
-            idx = 0
-            for i in range(len(std)):
-                if std[i][0] == t[row][col]:
-                    idx = i
-                    break
-
-            interested = get_interested_adjacent_tables(row, col, t, n, std[idx])
-            satisfaction = 0
-
-            if interested == 4:
-                satisfaction = 1000
-            elif interested == 3:
-                satisfaction = 100
-            elif interested == 2:
-                satisfaction = 10
-            elif interested == 1:
-                satisfaction = 1
-
-            s[row][col] = satisfaction
-            std.pop(idx)
+        return empty_tables, interested_tables
 
 
-allocates(students, table, N)
-calculate_satisfaction_table(students, table, N, satisfactions)
-
-total = 0
-for y in range(N):
-    total += sum(satisfactions[y])
-
-print(total)
+if __name__ == '__main__':
+    SharkSchool().solve()
