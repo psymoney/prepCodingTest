75ccb2fe1e5af158b9ea9dca7c4ee77685a22cb3
Sangyeop Kim <sangykimiv@gmail.com>
2023-08-13 17:27:52 +0900

    solve: 20164
    alternative way

--- BOJ/20164.py
+++ BOJ/20164.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 
 N = int(input())
 Q = deque([(0, N)])
-results = []
+answer = [10e9, 0]
 
 while len(Q) > 0: # O(2log N)
     v, n = deque.popleft(Q)
@@ -16,8 +16,10 @@ while len(Q) > 0: # O(2log N)
 
     for a in sn:
         sum += 1 if int(a) % 2 != 0 else 0
+
     if l == 1:
-        results.append(sum + v)
+        answer[0] = min(answer[0], sum + v)
+        answer[1] = max(answer[1], sum + v)
 
     elif l == 2:
         Q.append((v + sum, n // 10 + n % 10))
@@ -27,7 +29,6 @@ while len(Q) > 0: # O(2log N)
             for j in range(i + 1, l):
                 Q.append((v + sum, int(sn[:i]) + int(sn[i:j]) + int(sn[j:])))
 
-results.sort()  # O(log N)
-print(f'{results[0]} {results[-1]}')
+print(*answer)
 
 
