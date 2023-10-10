e8914f6c8381605b96ebda9e01450fe1ea0dd56d
Sangyeop Kim <sangykimiv@gmail.com>
2023-10-10 10:36:26 +0900

    update: 12015.py
    replace searching logic by method

--- BOJ/12015.py
+++ BOJ/12015.py
@@ -5,21 +5,27 @@ N = int(read())
 A = list(map(int, read().split()))
 lis = [A[0]]
 
+
+def b_search(t):
+    l, r = 0, len(lis) - 1
+
+    while l < r:
+        m = (l + r) // 2
+
+        if e > lis[m]:
+            l = m + 1
+        else:
+            r = m
+
+    return r
+
+
 for e in A[1:]:
     if e > lis[-1]:
         lis.append(e)
     else:
-        l, r = 0, len(lis) - 1
-
-        while l < r:
-            m = (l + r) // 2
-
-            if e > lis[m]:
-                l = m + 1
-            else:
-                r = m
-
-        lis[r] = e
+        i = b_search(e)
+        lis[i] = e
 
 print(len(lis))
 
