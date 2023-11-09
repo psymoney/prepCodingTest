51bf7c0ffbf1e22517d9f8b1b6d6bb721bb14392
Sangyeop Kim <sangykimiv@gmail.com>
2023-11-09 13:51:47 +0900

    update: BOJ_12933
    optimized algorithm
    
    O(n^2) -> O(n)

--- BOJ/java/BOJ_12933.java
+++ BOJ/java/BOJ_12933.java
@@ -3,56 +3,38 @@ package BOJ.java;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class BOJ_12933 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(solve(br.readLine()));
+    }
 
-        String sound = br.readLine();
-        String quack = "quack";
-        ArrayList<String> ducks = new ArrayList<>();
-
-        // if the length of input is not multiple of 5, it's not valid
+    static int solve(String sound) {
+        // if the length of input is not multiple of 5, the input is not valid
         if (sound.length() % 5 != 0) {
-            System.out.println(-1);
-            return;
+            return -1;
         }
-        
+
+        int[] ducks = new int[5];
+
         for (char c: sound.toCharArray()) {
-            int idx = quack.indexOf(c);
-            boolean isNotExist = true;
-            
-            for (int i=0; i < ducks.size(); i++) {
-                if (ducks.get(i).length() % 5 == idx) {
-                    ducks.set(i, ducks.get(i) + c);
-                    isNotExist = false;
-                    break;
-                }
-            }
-            
-            if (idx == 0 && isNotExist) {
-                ducks.add("q");
-            } else if (idx != 0 && isNotExist) {
-                System.out.println(-1);
-                return;
+            int idx = "quack".indexOf(c);
+
+            if (c == 'q') {
+                if (ducks[4] > 0) ducks[4]--;
+                ducks[0]++;
+            } else {
+                if (ducks[idx-1] == 0) return -1;
+                ducks[idx-1]--;
+                ducks[idx]++;
             }
         }
 
         // if all ducks is not finished by "quack" input is not valid
-        boolean r = true;
-        for (String duck: ducks) {
-            if (duck.length() % 5 != 0) {
-                r = false;
-                break;
-            }
-        }
+        if (ducks[0] > 0 || ducks[1] > 0 || ducks[2] > 0 || ducks[3] > 0) return -1;
 
-        if (r) {
-            System.out.println(ducks.size());
-        } else {
-            System.out.println(-1);
-        }
+        return ducks[4];
     }
 }
