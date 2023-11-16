c16e48c6adca85d558f1ecdee2ad521e593e09e6
Sangyeop Kim <sangykimiv@gmail.com>
2023-11-16 10:28:01 +0900

    fail: 13905
    맞왜틀?

--- BOJ/java/BOJ_13905.java
+++ BOJ/java/BOJ_13905.java
@@ -6,34 +6,24 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class BOJ_13905 {
-    static class Node {
-        private int maxWeight = 0;
-        private ArrayList<int> adjacent;
-        private ArrayList<int> costs;
+    static class Edge implements Comparable<Edge> {
+        int from;
+        int to;
+        int cost;
 
-        public Node(int adjacent, int cost) {
-            this.adjacent = new ArrayList<int>(List.of(adjacent));
-            this.costs = new ArrayList<int>(List.of(cost));
+        public Edge(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
         }
 
-        public void addAdjacent(int adjacent, int cost) {
-            this.adjacent.add(adjacent);
-            this.costs.add(cost);
-        }
-
-        public int updateMaxWeight(int weight) {
-            if(weight > maxWeight) {
-                maxWeight = weight;
-            }
-            return maxWeight;
-        }
-
-        public int getMaxWeight() {
-            return maxWeight;
+        @Override
+        public int compareTo(Edge o) {
+            return (this.cost - o.cost) * -1;
         }
     }
-
-    static private HashMap<Integer, Node> nodes;
+    static int[] parents;
+    static PriorityQueue<Edge> edges;
 
     public static void main(String[] args) throws IOException {
         // handle inputs
@@ -42,38 +32,54 @@ public class BOJ_13905 {
         int n = Integer.parseInt(firstInput.nextToken());
         int m = Integer.parseInt(firstInput.nextToken());
 
-        nodes = new HashMap<>();
+        parents = new int[n + 1];
+        for(int i = 0; i <= n; i++) parents[i] = i;
+        edges = new PriorityQueue<>();
 
         StringTokenizer secondInput = new StringTokenizer(br.readLine());
         int s = Integer.parseInt(secondInput.nextToken());
         int e = Integer.parseInt(secondInput.nextToken());
 
         for(int i = 0; i < m; i++) {
-            String[] input = br.readLine().split(" ");
-            if(nodes.containsKey(Integer.parseInt(input[0]))) {
-                nodes.get(Integer.parseInt(input[0])).addAdjacent(Integer.parseInt(input[1]), Integer.parseInt(input[2]));
-            } else {
-                nodes.put(Integer.parseInt(input[0]), new Node(Integer.parseInt(input[1]), Integer.parseInt(input[2])));
-            }
-            if(nodes.containsKey(Integer.parseInt(input[1]))) {
-                nodes.get(Integer.parseInt(input[1])).addAdjacent(Integer.parseInt(input[0]), Integer.parseInt(input[2]));
-            } else {
-                nodes.put(Integer.parseInt(input[1]), new Node(Integer.parseInt(input[0]), Integer.parseInt(input[2])));
-            }
+            char[] input = br.readLine().trim().replaceAll(" ", "").toCharArray();
+            edges.offer(new Edge(input[0] - '0', input[1] - '0', input[2] - '0'));
         }
+        // handle input ended
 
-
+        System.out.println(kruskal(s, e));
     }
 
-    private static void bfs(int s) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(s);
+    static int kruskal(int s, int e) {
+        int cost = 0;
+        while(!edges.isEmpty()) {
+            Edge current = edges.poll();
 
-        while(!q.isEmpty()) {
-            int n = q.poll();
-            Node current = nodes.get(n);
+            cost = current.cost;
+            int from = find(current.from);
+            int to = find(current.to);
 
+            if(from != to) {
+                union(from, to);
+            } else continue;
 
+            if(find(s) == find(e)) break;
+        }
+
+        if(parents[s] != parents[e]) cost = 0;
+
+        return cost;
+    }
+
+    static int find(int idx) {
+        if(idx == parents[idx]) return idx;
+        else return parents[idx] = find(parents[idx]);
+    }
+
+    static void union(int s, int e) {
+        if(s < e) {
+            parents[e] = s;
+        } else {
+            parents[s] = e;
         }
     }
 }
