5ac945fb72209613fb84209e5558ca02177d7cbf
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2024-04-08 21:05:49 +0900

    Time: 33 ms (74.61%), Space: 16.5 MB (90.09%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,16 +1,25 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        leftMin, leftMax = 0, 0
-
-        for c in s:
-            if c == "(":
-                leftMin, leftMax = leftMin + 1, leftMax + 1
-            elif c == ")":
-                leftMin, leftMax = leftMin - 1, leftMax - 1
+        stack = []
+        asterisks = []
+        
+        for i, c in enumerate(s):
+            if c == '(':
+                stack.append(i)
+            elif c == '*':
+                asterisks.append(i)
             else:
-                leftMin, leftMax = leftMin - 1, leftMax + 1
-            if leftMax < 0:
+                if stack:
+                    stack.pop()
+                elif asterisks:
+                    asterisks.pop()
+                else:
+                    return False
+                
+        while len(stack) and len(asterisks):
+            if stack.pop() > asterisks.pop():
                 return False
-            if leftMin < 0:
-                leftMin = 0
-        return leftMin == 0
+            
+        if len(stack) > 0:
+            return False
+        return True
