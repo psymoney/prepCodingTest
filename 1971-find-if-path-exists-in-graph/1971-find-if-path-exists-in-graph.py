496992b44ad8af0bf8d5238743fb72aef5673e7d
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2024-04-21 10:52:35 +0900

    Time: 1360 ms (97.52%), Space: 107.4 MB (85.17%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,46 +1,29 @@
-class Vertex:
-    def __init__(self, value: int) -> None:
-        self.value = value
-        self.visited = False
-        self.adjacents = []
-
-    def insert_adjacent(self, adjacent) -> None:
-        self.adjacents.append(adjacent)
-
-    def __eq__(self, other):
-        return self.value == other
-
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:     
+        if source == destination: return True
         vertices = {}
         
         for e1, e2 in edges:
-            if (e1 == source and e2 == destination) or (e1 == destination and e2 == source): return True
-            v1, v2 = vertices.get(e1), vertices.get(e2)
-            
-            if not v1:
-                v1 = Vertex(e1)
-                vertices.setdefault(e1, v1)
+            if (e1 == source and e2 == destination) or (e1 == destination and e2 == source): return True          
+            if not vertices.get(e1):
+                vertices.setdefault(e1, [])
                 
-            if not v2:
-                v2 = Vertex(e2)
-                vertices.setdefault(e2, v2)
-            
-            v1.insert_adjacent(v2)
-            v2.insert_adjacent(v1)
+            if not vertices.get(e2):
+                vertices.setdefault(e2, [])
+            vertices[e1].append(e2)
+            vertices[e2].append(e1)
         
-        if not vertices.get(source): return source == destination
-        
-        Q = [vertices.get(source)]
-        
+        visited = [False] * n
+        visited[source] = True
+        Q = [source]
         while Q:
             e = Q.pop()
             
-            for v in e.adjacents:
+            for v in vertices[e]:
                 if v == destination:
                     return True
-                if not v.visited:
-                    v.visited = True
+                if not visited[v]:
+                    visited[v] = True
                     Q.append(v)
                 
         return False
