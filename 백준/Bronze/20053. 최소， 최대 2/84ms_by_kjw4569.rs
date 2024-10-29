680a19acced9bedd6166a9666bfca67ebf894553
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2024-10-29 21:11:31 +0900

    Create 84ms_by_kjw4569.rs

---
+++ 백준/Bronze/20053. 최소， 최대 2/84ms_by_kjw4569.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+    let mut input = || input.next().unwrap();
+
+    const MAX: i32 = 1_000_000;
+
+    for _ in 0..input() {
+        let (min, max) = (0..input()).fold((MAX, -MAX), |(min, max), _| {
+            let num = input();
+            (num.min(min), num.max(max))
+        });
+
+        println!("{min} {max}");
+    }
+}
