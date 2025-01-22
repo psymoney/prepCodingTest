14be8707ba18885895cfb0dce6ada45309980493
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2025-01-22 22:10:49 +0900

    Time: 29 ms (28.57%) | Memory: 25.8 MB (28.57%) - LeetSync

---
+++ LeetCode/Medium/1876-map-of-highest-peak/map-of-highest-peak.rs
@@ -0,0 +1,39 @@
+use std::collections::VecDeque;
+
+impl Solution {
+    pub fn highest_peak(is_water: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let rows = is_water.len();
+        let cols = is_water[0].len();
+        let mut deq: VecDeque<(usize, usize)> = VecDeque::with_capacity(rows * cols);
+        let mut distance: Vec<Vec<i32>> = vec![vec![-1; cols]; rows];
+        let directions: [(i32, i32); 4] = [(0, 1), (0, -1), (-1, 0), (1, 0)];
+
+        for y in 0..rows {
+            for x in 0..cols {
+                if is_water[y][x] == 1 {
+                    deq.push_back((x, y));
+                    distance[y][x] = 0;
+                }
+            }
+        }
+
+        while let Some((x, y)) = deq.pop_front() {
+            for (dx, dy) in directions {
+                let nx = x as i32 + dx;
+                let ny = y as i32 + dy;
+
+                if nx >= 0 && ny >= 0 && nx < cols as i32 && ny < rows as i32 {
+                    let nx = nx as usize;
+                    let ny = ny as usize;
+
+                    if distance[ny][nx] == -1 {
+                        distance[ny][nx] = distance[y][x] + 1;
+                        deq.push_back((nx, ny));
+                    }
+                }
+            }
+        }
+
+        distance
+    }
+}
